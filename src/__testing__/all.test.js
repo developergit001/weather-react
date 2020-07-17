9dc8964aa4d9cb1d82e931cb35bb350a89f6c5ca
Pablo <caraballopablo@gmail.com>
2020-07-16 23:18:30 -0300

    fix mock test

--- src/__testing__/all.test.js
+++ src/__testing__/all.test.js
@@ -4,7 +4,24 @@ import {render, cleanup, waitForElement} from '@testing-library/react';
 import Weather from "../components/weather";
 import { getCentrigrados, getWeather, getWeatherEndPoint } from '../components/utilities/utils';
 import Main from '../components/main';
-import mockAxios from "../__mocks__/axios";
+
+/*
+If you want that all your tests (each one of them) use the fake mock axios 
+then USE:
+Choice A)
+import mockAxios from "../__mocks__/axios"; //for global response with exact the same json inside here!
+
+otherwise USE this 2 lines:
+
+CHOICE B)
+import axios from "axios";
+jest.mock('axios');
+*/
+
+//CHOICE B)
+import axios from "axios";
+jest.mock('axios');
+
 
 afterEach(cleanup);
 
@@ -22,21 +39,46 @@ PRUEBAS: Este archivo tiene realizada todas las posibles pruebas a modo de ejemp
 it('Prueba de mock fake testing simulando ajax request', async () => {
 
   //Dato de respuesta emulado
-  const resdata = {"cod":0,"entity":{"coord":{"lon":12.48,"lat":41.89},"weather":[{"id":800,"main":"Clear","description":"cielo claro","icon":"01d"}],"base":"stations","main":{"temp":295.62,"feels_like":296.88,"temp_min":294.26,"temp_max":298.15,"pressure":1011,"humidity":83},"visibility":10000,"wind":{"speed":3.1,"deg":130},"clouds":{"all":0},"dt":1593748201,"sys":{"type":1,"id":6792,"country":"IT","sunrise":1593747586,"sunset":1593802118},"timezone":7200,"id":3169070,"name":"Rome","cod":200}};
+  const resdata = {data:{"cod":0,"mockaxiostest":true,"entity":{"coord":{"lon":12.48,"lat":41.89},"weather":[{"id":800,"main":"Clear","description":"cielo claro","icon":"01d"}],"base":"stations","main":{"temp":295.62,"feels_like":296.88,"temp_min":294.26,"temp_max":298.15,"pressure":1011,"humidity":83},"visibility":10000,"wind":{"speed":3.1,"deg":130},"clouds":{"all":0},"dt":1593748201,"sys":{"type":1,"id":6792,"country":"IT","sunrise":1593747586,"sunset":1593802118},"timezone":7200,"id":3169070,"name":"Rome","cod":200}}};
   // setup
+  /*
+  //CHOICE A
   mockAxios.get.mockImplementationOnce(() =>
     Promise.resolve(resdata)
   );
-    const cityId = "3169070"; //pasamos como city a Roma.
-    const datos = await getWeather(cityId);
-    expect(datos).toEqual(resdata);
-    expect(mockAxios.get).toHaveBeenCalledTimes(1);
-    let endpointurl = getWeatherEndPoint() + "/" + cityId;
+  */
 
-    expect(mockAxios.get).toHaveBeenCalledWith(
-      endpointurl
-      //,{params: {cityId:cityId}}
-    );
+  /*
+  //First, by default the new mock axios return undefined, you must put a default data return.
+
+  axios.get('https://www.google.com'); //undefined!!!
+  const datos = await getWeather(cityId); //undefined too!!!
+  */
+
+  //Doing this we put a default value for mock
+  axios.get.mockResolvedValue(
+    resdata //The data that i must test
+  );
+
+
+  const cityId = "3169070"; //pasamos como city a Roma.
+  let endpointurl = getWeatherEndPoint() + "/" + cityId;
+
+  const datos = await getWeather(cityId);
+  
+  //console.log("mock result",datos); //Call the mock axios yeah!!!
+  
+  expect(datos).toEqual(resdata);
+  
+  //IF we use __mock__/axios.js in every test, replace the real axios for the fake one
+  //__mock__/axios.js has a default data too, must be the correct want for every test, be carefull.
+  //CHOICE A)
+  //expect(mockAxios.get).toHaveBeenCalledTimes(1);
+  //expect(mockAxios.get).toHaveBeenCalledWith(endpointurl /*,{params: {cityId:cityId}}*/);
+  
+  //CHOICE B) only for this test.
+  expect(axios.get).toHaveBeenCalledTimes(1);
+  expect(axios.get).toHaveBeenCalledWith(endpointurl /*,{params: {cityId:cityId}}*/);
 
 });
 
